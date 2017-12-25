dec38578b90dc887605c291408d790e5d58d4c54
Deepak M <deepak@socash.sg>
2017-12-25 15:53:44 +0530

    Add a spinner while the image is loading

--- DefaultSlide.js
+++ DefaultSlide.js
@@ -7,18 +7,32 @@ import {
   Dimensions,
   Platform,
 } from 'react-native';
+import { Spinner } from 'native-base';
 
 export default class DefaultSlide extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false,
+    };
+  }
+
+  onImageLoad = () => {
+    this.setState({ loaded: true });
+  };
+
   render() {
     const style = {
       backgroundColor: this.props.backgroundColor,
       paddingTop: this.props.topSpacer,
       paddingBottom: this.props.bottomSpacer,
-    }
+    };
+
     return (
       <View style={[styles.mainContent, style]}>
         <Text style={styles.title}>{this.props.title}</Text>
-        <Image source={this.props.image} style={this.props.imageStyle} />
+        <Image source={this.props.image} style={this.props.imageStyle} onLoad={this.onImageLoad} />
+        {!this.state.loaded && <Spinner style={this.props.imageStyle} color="#000000" />}
         <Text style={styles.text}>{this.props.text}</Text>
       </View>
     );
